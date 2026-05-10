1dfb6ee7005a4359b3a2424976716e4bfae591ba
rifat76511 <rifatalvi765@gmail.com>
2026-05-10 11:22:19 +0600

    port chnage and add

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,7 +1,8 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
-
+import dns from "node:dns/promises";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db("book-borrow");
 
